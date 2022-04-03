381a2c1aae15d4b7505ab683566cf22269b214cc
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-02 19:11:49 -0700

    work on the html and css

---
+++ script.js
@@ -0,0 +1,18 @@
+ //APi key for weather
+ var weatherAPI = "7f41dda623830e062579b1c56271149f"
+ 
+ //search container elements
+ var searchContainer = document.getElementById("searchContainer");
+ var searchButton = document.getElementsByClassName("btn")
+
+ //current city searched container
+ var currentCity = document.getElementsByClassName("currentWeather");
+ var searchedCity = document.getElementById("searchedCity");
+
+ // search result container
+var searchResults = document.getElementsByClassName("searchResults");
+
+//five day forecast container
+var fiveDayForecast = document.getElementById("fiveDayForecast")
+
+
