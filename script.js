8fdcde6505b6f602ccd9e3304c79cdaba8c30981
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-08 16:16:46 -0700

    working on the 5 day forecast

--- script.js
+++ script.js
@@ -22,12 +22,19 @@ var tempValue= document.querySelector(".temp");
 var wind = document.getElementById("wind");
 var windValue= document.querySelector(".wind");
 var humidity = document.getElementById("humidity");
-var humidityvalue = document.querySelector(".humidity")
+var humidityValue = document.querySelector(".humidity")
 var uvIndex = document.getElementById("uvIndex");
 var uvIndexValue = document.querySelector(".uvIndex")
 
-var cityName = "seattle"
+var date1 = document.querySelector(".date1")
+var date1Icon = document.querySelector(".date1Icon")
+var date1Temp = document.querySelector(".date1Temp")
+var date1Wind = document.querySelector(".date1Wind")
+var date1Humidity = document.querySelector(".date1Humidity")
 
+
+var cityName = "seattle"
+//callin the APIs to get all the info to populate the page
 function getWeather(cityName) {
     console.log("getting weather")
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
@@ -41,16 +48,22 @@ function getWeather(cityName) {
     console.log(lat, lon)
        tempValue.innerHTML= data.main.temp;
        windValue.innerHTML= data.wind.speed;
-       uvIndexValue.innerHTML = data.main.humidity;
-
+       humidityValue.innerHTML = data.main.humidity;
+//second API getting called to get the UVindex and 5 day weather
        let UVQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
-       
+
        fetch(UVQueryURL)
        .then(function(response){
            return response.json()
        }).then(function(data){
            console.log("data: ", data)
+        uvIndexValue.innerHTML = data.daily[0].uvi;
+        //info for the second day
+        date1Temp.innerHTML = data.daily[1].temp.day;
+        date1Wind.innerHTML = data.daily[1].wind_speed
+        date1Humidity.innerHTML = data.daily[1].humidity
        })
+      
     })
     
 }
